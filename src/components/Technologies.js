6042340a8ce28f740a6381d36ad1cce103106a74
bboubacar <bboubacarfr@gmail.com>
2024-02-14 18:01:00 +0100

    Première version

---
+++ src/components/Technologies.js
@@ -0,0 +1,10 @@
+import BodyPanel from "./BodyPanel";
+import technologies from "../json/technologie.json";
+
+function Technologies({id}) {
+    return(
+       <BodyPanel mainTitle="Technologies" items={technologies} id={id} />
+    )
+}
+
+export default Technologies;
